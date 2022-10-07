d190085797e90d0616cefbd0284fb25022bd0599
KODA97 <acovamartinez@gmail.com>
2022-10-07 11:43:01 -0500

    link
    Modificaciones de links

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Alumno from "./comp/Alumno";
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //------------Variables-----------------//
